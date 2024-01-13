a1143de6415cd78ed84c127f80dca48355d0c84d
nickrmurphy <hi@nickmurphy.io>
2024-01-13 11:32:26 -0600

    Layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -28,41 +28,43 @@ export default function App() {
   }, []);
 
   return (
-    <div className="sm:px-6 lg:px-8 px-0 inset-x-0 py-10 fixed sm:inset-x-10 md:inset-x-16 lg:inset-x-24 xl:inset-x-36 justify-center bg-white h-full border shadow gap-y-5">
+    <div className="sm:px-6 lg:px-8 px-0 inset-x-0 py-10 fixed sm:inset-x-10 md:inset-x-16 lg:inset-x-24 xl:inset-x-36 justify-center bg-white h-full border shadow gap-y-5 overflow-auto">
       <div className="text-center gap-y-10">
         <h1 className="text-4xl font-extrabold row-auto font-display">Nick Murphy</h1>
         <p className="mt-2 text-lg leading-8 text-gray-600">
           Software Engineer
         </p>
         {/* Links */}
-        <nav className="isolate flex divide-x divide-gray-200 rounded-full m-2 border shadow-sm" aria-label="Tabs">
-          {LINKS.map((link, idx) => (
-            <a
-              key={link.name}
-              href={link.url}
-              target={link.current ? '' : '_blank'}
-              className={classNames(
-                idx === 0 ? 'rounded-l-full' : '',
-                idx === LINKS.length - 1 ? 'rounded-r-full' : '',
-                'group relative min-w-0 flex-1 overflow-hidden p-2 text-center text-sm font-medium hover:bg-slate-50 text-gray-600 hover:text-gray-900'
-              )}
-            >
-              <span>{link.name}</span>
-            </a>
-          ))}
-        </nav>
+        <div className="max-w-screen-md mx-auto">
+          <nav className="isolate flex divide-x divide-gray-200 rounded-full m-2 border shadow-sm" aria-label="Tabs">
+            {LINKS.map((link, idx) => (
+              <a
+                key={link.name}
+                href={link.url}
+                target={link.current ? '' : '_blank'}
+                className={classNames(
+                  idx === 0 ? 'rounded-l-full' : '',
+                  idx === LINKS.length - 1 ? 'rounded-r-full' : '',
+                  'group relative min-w-0 flex-1 overflow-hidden p-2 text-center text-sm font-medium hover:bg-slate-50 text-gray-600 hover:text-gray-900'
+                )}
+              >
+                <span>{link.name}</span>
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
       {/* Content */}
-      <div className="mx-5 mt-10 gap-y-5 items-center justify-center">
+      <div className="mx-5 mt-10 gap-y-5 justify-center flex">
         {/* About */}
-        <div className="max-w-prose">
-          <h2 className="text-xl font-semibold">About Me</h2>
-          <p className="mt-2 text-md  font-light leading-8 text-gray-600">
-            I am a Software Engineer. I learned to write code in 2016, when I self-taught with the help of The Internet. I learned to how software worked while studying at Lewis Universty to attain a Bachelor’s degree in Computer Science in 2019. I learned how to build software products in the years since my first full time job as a developer shortly after. In the coming years, I aspire to learn how to build teams and businesses.
-            <br />
-            <br />
-            To give the left brain a rest and wind down the analytical and plan-oriented bits of my mind, I improvise! Improv comedy, that is. It’s a great way to support creativity and mental health, while being active in a community I love — and hopefully make a few people laugh!
-          </p>
+          <div className="max-w-prose">
+            <h2 className="text-xl font-semibold">About Me</h2>
+            <p className="mt-2 text-md  font-light leading-8 text-gray-600">
+              I am a Software Engineer. I learned to write code in 2016, when I self-taught with the help of The Internet. I learned to how software worked while studying at Lewis Universty to attain a Bachelor’s degree in Computer Science in 2019. I learned how to build software products in the years since my first full time job as a developer shortly after. In the coming years, I aspire to learn how to build teams and businesses.
+              <br />
+              <br />
+              To give the left brain a rest and wind down the analytical and plan-oriented bits of my mind, I improvise! Improv comedy, that is. It’s a great way to support creativity and mental health, while being active in a community I love — and hopefully make a few people laugh!
+            </p>
         </div>
       </div>
     </div>
